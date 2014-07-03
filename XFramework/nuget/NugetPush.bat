cee20390418bafe2fbdf279a5b8d9ed122fef372
Pippon <xiulei77@gmail.com>
2014-07-03 14:44:59 +0800

    nuget

---
+++ XFramework/nuget/NugetPush.bat
@@ -0,0 +1,7 @@
+@echo on
+echo "start pack"
+..\..\.nuget\nuget.exe pack ..\XFramework.csproj
+echo "start upload'"
+..\..\.nuget\nuget.exe setApiKey cf555ff7-a9c6-4720-966a-6f7f1b36d29f
+..\..\.nuget\nuget.exe push XFramework.1.0.2.0.nupkg
+pause
