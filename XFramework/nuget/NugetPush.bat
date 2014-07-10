c17c3a3eee93e0187de16500b1269a58e342f1eb
Pippon <xiulei77@gmail.com>
2014-07-10 13:09:57 +0800

    framework4.5变回4.0

--- XFramework/nuget/NugetPush.bat
+++ XFramework/nuget/NugetPush.bat
@@ -3,5 +3,5 @@ echo "start pack"
 ..\..\.nuget\nuget.exe pack ..\XFramework.csproj
 echo "start upload'"
 ..\..\.nuget\nuget.exe setApiKey cf555ff7-a9c6-4720-966a-6f7f1b36d29f
-..\..\.nuget\nuget.exe push XFramework.1.0.2.0.nupkg
+..\..\.nuget\nuget.exe push XFramework.1.0.2.2.nupkg
 pause
